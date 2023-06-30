1e84df58d35ec74b8395a51b7f789a1ed045e818
Geisonm161 <97369708+Geisonm161@users.noreply.github.com>
2023-06-29 23:52:34 -0400

    Proyecto en proceso 1

--- src/view/Landing/Landing.js
+++ src/view/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import style from "./Landing.module.scss";
 
@@ -6,21 +6,22 @@ function Landing() {
   const [accessLanding, setAccessLanding] = useState(true);
   const [deleteContainer, setDeleteContainer] = useState(false);
 
-  setTimeout(() => {
-    setAccessLanding(false);
+  useEffect(() => {
     setTimeout(() => {
-      setDeleteContainer(true);
-    }, 3000);
-  }, 4000);
-
+      setAccessLanding(false);
+      setTimeout(() => {
+        setDeleteContainer(true);
+      }, 3000);
+    }, 4000);
+  });
   return (
     <div
       className={` ${
         accessLanding ? style.containerLanding : style.containerMove
       } ${deleteContainer && style.deleteContainer}`}
     >
-      <PacmanLoader 
-        color={'rgb(163, 163, 163)'}
+      <PacmanLoader
+        color={"rgb(163, 163, 163)"}
         loading={true}
         size={90}
         aria-label="Loading Spinner"
